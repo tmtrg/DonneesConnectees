2ccb34788e57b6e4e85ffb0da3a0fb0e9562a363
Thuy My TRUONG <thuy.my.truong@outlook.fr>
2023-03-15 14:43:50 +0100

    gjf

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static('html'));
 
 app.use((req, res, next)=>{
 	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Methods', 'GET, PUT? POST, DELETE');
+	res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
 	res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, x-Requested-With');
 	next();
 });
