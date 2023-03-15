432aac895a25bbbbb1e64dcafbb135ead4660f6a
Thuy My TRUONG <thuy.my.truong@outlook.fr>
2023-03-15 11:23:37 +0100

    modifications

--- index.js
+++ index.js
@@ -6,47 +6,65 @@ const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+var cors = require('cors');
+
 var data={};
 var id=0;
 
 app.use(express.static('html'));
 
+app.use((req, res, next)=>{
+	res.header('Acces-Control-Allow-Origin', '*');
+	res.header('Acces-Control-Allow-Methods', 'POST');
+	res.header('Acces-Control-Allow-Headers', 'Content-Type');
+	next();
+});
+
+
+
 	
-app.post("/annotation", function(req, res){
+app.post("/annotation", cors(), function(req, res){
 	var body = req.body;
 	// console.log(body);
 	data[id]=body;
 	// data.push(body);
 	console.log(data);
 	id++;
-	res.send();
+	res.send("Votre commentaire a bien été pris en compte et porte l'identifiant "+(id-1));
 });
 
 
-app.get("/IdAnnot", function(req, res){
-	var IdAnnot = req.query.Annot;
+app.get("/IdAnnot/:Annot", function(req, res){
+	// var IdAnnot = req.query.Annot;
+	var IdAnnot = req.params.Annot;
+	
 	
 	var Exist=Object.keys(data).includes(IdAnnot);
 	
-	var ChoixFormat=req.query.FormatIdAnnot;
+	// var ChoixFormat=req.query.FormatIdAnnot;
 	
-	console.log(req.headers['accept']);
+	// console.log(req);
 	
-	if (ChoixFormat=="html"){
-		req.headers['accept']= 'text/html';
-	}
-	else {
-		if (ChoixFormat=="Json"){
-			req.headers['accept']=  'application/json';
-		}	
-	}
+	// console.log(req.headers['accept']);
 	
-	console.log(req.headers['accept']);
+	// if (ChoixFormat=="html"){
+		// req.headers['accept']= 'text/html';
+	// }
+	// else {
+		// if (ChoixFormat=="json"){
+			// req.headers['accept']=  'application/json';
+		// }	
+	// }
+	
+	// console.log(req.headers['accept']);
 	
 	res.format ({
 		   'text/html': function() {
+			    res.setHeader('Content-Type', 'text/html');
 			    if (Exist){
-				   res.send(data[IdAnnot]); 
+					res.setHeader('Content-Type', 'text/html');
+				    res.send("<!DOCTYPE html><html lang='fr'><head><meta charset='UTF-8'/><title>Titre</title></head><body><div>"+JSON.stringify(data[IdAnnot])+
+							"</div></body></html>"); 
 			    }
 			    else {
 				   res.send("aucune annotation n'est associée à cette clé");
@@ -54,8 +72,9 @@ app.get("/IdAnnot", function(req, res){
 		   },
 
 		   'application/json': function() {
+			    res.setHeader('Content-Type', 'application/json');
 			    if (Exist){
-				   res.send(data[IdAnnot]); 
+				    res.send(data[IdAnnot]); 
 			    }
 			    else {
 				   res.send("aucune annotation n'est associée à cette clé");
@@ -68,63 +87,117 @@ app.get("/IdAnnot", function(req, res){
 
 
 
-app.get("/AllAnnot", function(req, res){
-	var ChoixFormat=req.query.FormatAllAnnot;
+// app.get("/AllAnnot", function(req, res){
+	
+	// res.format ({
+		   // 'text/html': function() {
+				// res.setHeader('Content-Type', 'text/html');
+				// res.send("<!DOCTYPE html><html lang='fr'><head><meta charset='UTF-8'/><title>Titre</title></head><body><div>"+JSON.stringify(data)+
+						// "</div></body></html>"); 
+		   // },
+
+		   // 'application/json': function() {
+			    // res.setHeader('application/json');
+				// res.send(data); 
+			// }
+	// });
+	
+	// var ChoixFormat=req.query.FormatAllAnnot;
 	
 	
-	if (ChoixFormat=="html"){
-		req.headers['accept']= 'text/html';
-	}
-	else {
-		if (ChoixFormat=="Json"){
-			req.headers['accept']=  'application/json';
-		}	
-	}
+	// if (ChoixFormat=="html"){
+		// req.headers['accept']= 'text/html';
+	// }
+	// else {
+		// if (ChoixFormat=="json"){
+			// req.headers['accept']=  'application/json';
+		// }	
+	// }
 		
-	res.format ({
-		   'text/html': function() {
-			  res.send(data); 
-		   },
+	// res.format ({
+		   // 'text/html': function() {
+			    // res.setHeader('Content-Type', 'text/html');
+				// res.send("<!DOCTYPE html><html lang='fr'><head><meta charset='UTF-8'/><title>Titre</title></head><body><div>"+JSON.stringify(data)+
+						// "</div></body></html>"); 
+		   // },
 
-		   'application/json': function() {
-			  res.send(data);
-			}
-	});
+		   // 'application/json': function() {
+			  // res.setHeader('application/json');
+			  // res.send(data);
+			// }
+	// });
 	
-});
+// });
 
 
 app.get("/URI", function(req, res){
 	var IdURI = req.query.AnnotURI;
+	// var IdURI = req.params.AnnotURI;
+	console.log(IdURI);
 	
 	var ChoixFormat=req.query.FormatAnnotURI;
-	
-	var tabRep=[]
-	
-	for (key in data){
-		if (data[key]["URI"]==IdURI){
-			tabRep.push({"IdAnnotation" : data[key], "Commentaire" : data[key]["Commentaire"]});
-		}
-	}
 	
 	if (ChoixFormat=="html"){
 		req.headers['accept']= 'text/html';
 	}
 	else {
-		if (ChoixFormat=="Json"){
+		if (ChoixFormat=="json"){
 			req.headers['accept']=  'application/json';
 		}	
 	}
-		
-	res.format ({
+	
+	if (IdURI=="AllInfo"){
+		res.format ({
 		   'text/html': function() {
-			  res.send(tabRep); 
+				res.setHeader('Content-Type', 'text/html');
+				res.send("<!DOCTYPE html><html lang='fr'><head><meta charset='UTF-8'/><title>Titre</title></head><body><div>"+JSON.stringify(data)+
+						"</div></body></html>"); 
 		   },
 
 		   'application/json': function() {
-			  res.send(tabRep);
+			    res.setHeader('Content-Type','application/json');
+				res.send(data); 
 			}
-	});
+		});
+	}else {
+	
+		var tabRep=[];
+		
+		for (key in data){
+			// console.log(key);
+			if (data[key]["URI"]==IdURI){
+				tabRep.push({"IdAnnotation" : key, "Commentaire" : data[key]["Commentaire"]});
+			}
+		}
+		
+		console.log(tabRep);
+		
+		// res.format ({
+			   // 'text/html': function() {
+					// res.setHeader('Content-Type', 'text/html');
+					// res.send("<!DOCTYPE html><html lang='fr'><head><meta charset='UTF-8'/><title>Titre</title></head><body><div>"+JSON.stringify(tabRep)+
+							// "</div></body></html>"); 
+			   // },
+
+			   // 'application/json': function() {
+					// res.send(tabRep); 
+				// }
+		// });
+			
+		res.format ({
+			   'text/html': function() {
+					res.setHeader('Content-Type', 'text/html');
+					res.send("<!DOCTYPE html><html lang='fr'><head><meta charset='UTF-8'/><title>Titre</title></head><body><div>"+JSON.stringify(tabRep)+
+							"</div></body></html>"); 
+			   },
+
+			   'application/json': function() {
+					res.setHeader('Content-Type','application/json');
+					res.send(tabRep); 
+				}
+		});
+	
+	}
 	
 });
 
